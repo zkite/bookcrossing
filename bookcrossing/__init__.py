f0f992e126fffc3b6cb19c1f2e1c889165bb2034
DmytroKaminskiy <dmytro.kaminskyi92@gmail.com>
2016-06-14 16:27:31 +0300

    merge master with plark

--- bookcrossing/__init__.py
+++ bookcrossing/__init__.py
@@ -1,14 +1,10 @@
 import config
 
 from flask import Flask
-# from flask_login import Logging
 from flask_restful import Api
 from flask_bootstrap import Bootstrap
 
-
-
-from bookcrossing.resources import (resources,
-                                    book_request_resources)
+from bookcrossing.resources import book_request_resources
 from bookcrossing.models.models import (login_manager,
                                         db)
 from bookcrossing.mail import mail
@@ -28,30 +24,30 @@ app.config.from_object(config.DevelopmentConfig)
 
 api = Api(app, prefix='/bookcrossing/v1')
 
-# flask_log = Logging(app)
-# with app.app_context():
-#     db.create_all()
 # engine = config.DevelopmentConfig.create_psql_engine()
-
-#  Register your urls here
-api.add_resource(resources.Index, '/', '/index')
-
-api.add_resource(book_request_resources.BookRequestResource,
-                 '/book-request/<int:book_id>/<int:requester_id>',
-                 '/book-request/<int:request_id>')
-
 
 bootstrap = Bootstrap()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'login'
 
 db.init_app(app)
+with app.app_context():
+    db.create_all()
+
 bootstrap.init_app(app)
+
 mail.init_app(app)
+
 login_manager.init_app(app)
 
-
+#  Register your urls here
 app.add_url_rule('/', 'hello', hello)
 app.add_url_rule('/registration', 'registration', registration, methods=['GET', 'POST'])
 app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
 app.add_url_rule('/logout', 'logout', logout)
+
+
+#  Register your REST urls here
+api.add_resource(book_request_resources.BookRequestResource,
+                 '/book-request/<int:book_id>/<int:requester_id>',
+                 '/book-request/<int:request_id>')
