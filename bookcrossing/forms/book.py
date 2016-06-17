6db5dd48a4392fd484d844338e39f76b898fdedf
zkite <kyryll.orlov@gmail.com>
2016-06-17 12:10:11 +0300

    change length input for book: title, author, publisher, category

--- bookcrossing/forms/book.py
+++ bookcrossing/forms/book.py
@@ -3,13 +3,13 @@ from wtforms.validators import Length, InputRequired
 
 class AddBookForm(Form):
 	title = StringField('Title', [InputRequired('Title is required'),
-	                              Length(min=6, max=30, message='Title must have from 6 to 30 characters')])
+	                              Length(min=4, max=30, message='Title must have from 6 to 30 characters')])
 	author = StringField('Author', [InputRequired('Author is required'),
-	                              Length(min=6, max=30, message='Author must have from 6 to 30 characters')])
+	                              Length(min=4, max=30, message='Author must have from 6 to 30 characters')])
 	publisher = StringField('Publisher', [InputRequired('Publisher is required'),
-	                              Length(min=6, max=30, message='Publisher must have from 6 to 30 characters')])
+	                              Length(min=4, max=30, message='Publisher must have from 6 to 30 characters')])
 	category = StringField('Category', [InputRequired('Category is required'),
-	                              Length(min=6, max=30, message='Category must have from 6 to 30 characters')])
+	                              Length(min=4, max=30, message='Category must have from 6 to 30 characters')])
 
 
 class UpdateBookForm(AddBookForm):
