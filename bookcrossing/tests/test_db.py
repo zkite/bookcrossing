627161e5d55c5bcd3e12733281c72c7439657957
DmytroKaminskiy <dmytro.kaminskyi92@gmail.com>
2016-06-27 21:33:00 +0300

    split tests by CRUD

--- bookcrossing/tests/test_db.py
+++ bookcrossing/tests/test_db.py
@@ -16,36 +16,38 @@ class TestDataBase(TestCase):
     def create_app(self):
         app = Flask(__name__)
         app.config.from_object(TestingConfig)
-
         db.init_app(app)
         with app.app_context():
             db.create_all()
-
         return app
 
     def setUp(self):
-        test_user_1 = UserModel('MayerLogin', 'Mayerpassword', 'mayer@gmail.com',
-                                'Mayer', 'Hawthorne', 'Dnepr_office', '12345678900')
-
-        test_user_2 = UserModel('JakeLogin', 'Jakepassword', 'jake@gmail.com',
-                                'Jake', 'Black', 'Lviv_office', '09876543211')
+        test_user_1 = UserModel(login='MayerLogin',
+                                password='Mayerpassword',
+                                email='mayer@gmail.com',
+                                first_name='Mayer',
+                                last_name='Hawthorne',
+                                office='Dnepr_office',
+                                phone_number='12345678900')
 
         db.session.add(test_user_1)
-        db.session.add(test_user_2)
         db.session.commit()
 
-    def test_users(self):
+    def test_user(self):
         user_1 = UserModel.query.get(1)
-        user_2 = UserModel.query.get(2)
 
-        self.assertNotEqual(user_1.id, user_2.id)
         self.assertIn(user_1, db.session)
-        self.assertIn(user_2, db.session)
         self.assertEqual(user_1.login, 'MayerLogin')
-        self.assertEqual(user_2.login, 'JakeLogin')
         self.assertEqual(user_1.email, 'mayer@gmail.com')
-        self.assertEqual(user_2.email, 'jake@gmail.com')
+        self.assertEqual(user_1.first_name, 'Mayer')
+        self.assertEqual(user_1.last_name, 'Hawthorne')
+        self.assertEqual(user_1.office, 'Dnepr_office')
+        self.assertEqual(user_1.phone_number, '12345678900')
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
