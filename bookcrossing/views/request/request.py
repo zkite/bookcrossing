a42e5d487f6168017f4838a0fcd30a1537d53069
MaxShumer <maxstolovich@gmail.com>
2016-07-01 08:51:43 +0300

    Added logging config dict and logging in request view

--- bookcrossing/views/request/request.py
+++ bookcrossing/views/request/request.py
@@ -1,51 +1,61 @@
 from datetime import datetime
 from flask_login import current_user
+import logging
+import logging.config
 
 from bookcrossing.views.request.base_request import BaseRequestView
 from bookcrossing.models.book import BookModel
+from bookcrossing.config import LOGGING
+
+logging.config.dictConfig(LOGGING)
 
 
 class RequestView(BaseRequestView):
     def get(self, request_id=None):
         if not request_id:
-            return 'RequestView GET request_id ERROR'
+            logging.error('RequestView GET request_id ERROR')
         request_obj = self.get_request(request_id)
+        logging.info('Request:{}'.format(request_obj))
         if not request_obj:
-            return 'RequestView GET request_obj ERROR'
-        return 'RequestView GET request_obj OK'
+            logging.error('RequestView GET request_obj ERROR')
+        logging.info('RequestView GET request_obj OK')
 
     def post(self, book_id=None):
         if not book_id:
-            return 'RequestView POST book_id ERROR'
+            logging.error('RequestView POST book_id ERROR')
         if not current_user:
-            return 'RequestView POST current_user ERROR'
+            logging.error('RequestView POST current_user ERROR')
         book = self.get_model(book_id,
                               BookModel)
+        logging.info('Book:{}'.format(book))
         if not book:
-            return 'RequestView POST Book ERROR'
+            logging.error('RequestView POST Book ERROR')
         data = {'book_id': book.id,
                 'req_user_id': current_user.id,
                 'owner_user_id': book.user_id}
         book_request = self.create_request(request_data=data,
                                            uid=current_user.id)
+        logging.info('Book request:{}'.format(book_request))
         if not book_request:
-            return 'RequestView POST book_request ERROR'
-        return 'RequestView POST book_request OK'
+            logging.error('RequestView POST book_request ERROR')
+        logging.info('RequestView POST book_request OK')
 
     def put(self, request_id=None):
         if not request_id:
-            return 'RequestView PUT request_id ERROR'
+            logging.error('RequestView PUT request_id ERROR')
         data = {'accept_date': datetime.now()}
         update_request = self.update_request(rid=request_id,
                                              request_data=data)
+        logging.info('Updated request:{}'.format(update_request))
         if not update_request:
-            return 'RequestView PUT update_request ERROR'
-        return 'RequestView PUT update_request OK'
+            logging.error('RequestView PUT update_request ERROR')
+        logging.info('RequestView PUT update_request OK')
 
     def delete(self, request_id=None):
         if not request_id:
-            return 'RequestView DELETE request_id ERROR'
+            logging.error('RequestView DELETE request_id ERROR')
         delete_request = self.delete_request(rid=request_id)
+        logging.info('Deleted request:{}'.format(delete_request))
         if not delete_request:
-            return 'RequestView DELETE delete_request ERROR'
-        return 'RequestView DELETE delete_request OK'
+            logging.error('RequestView DELETE delete_request ERROR')
+        logging.info('RequestView DELETE delete_request OK')
